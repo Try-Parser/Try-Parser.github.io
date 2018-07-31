b3d025332f41957d45710038bf89dad6b504feef
Franklin Embate <ferox.dragon@gmail.com>
2018-07-31 09:56:43 +0800

    init

---
+++ precache-manifest.e76eef77f7d474b0b1a7c44ea5447d1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ff186c0efeae5309689d",
+    "url": "/js/chunk-vendors.ff186c0e.js"
+  },
+  {
+    "revision": "afb200570bacd81d866c",
+    "url": "/js/app.afb20057.js"
+  },
+  {
+    "revision": "52fd1eb1e25abefbac36e680f887fdc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ff186c0efeae5309689d",
+    "url": "/css/chunk-vendors.b8dd551b.css"
+  },
+  {
+    "revision": "afb200570bacd81d866c",
+    "url": "/css/app.83b07a1e.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.e76eef77f7d474b0b1a7c44ea5447d1d.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "@try-parser/fte-helper"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
